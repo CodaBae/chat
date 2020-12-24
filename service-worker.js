bc28a626347e94ba9820ca4fc9e4b92aff47c327
shalom.mat40@gmail.com <shalom.mat40@gmail.com>
2020-12-24 16:45:02 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chatting/precache-manifest.786b8380e4281fa605df7ce04c4cdd97.js"
+  "/chatting/precache-manifest.e47da9086698201540f10f3484e05e7f.js"
 );
 
 self.addEventListener('message', (event) => {
