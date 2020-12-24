6248797112ab5210cac8270f49d6ac4a22c15c49
shalom.mat40@gmail.com <shalom.mat40@gmail.com>
2020-12-24 15:40:12 +0100

    Updates

---
+++ precache-manifest.128c8b0518f1127bef3879e27a1b7a9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dcef660b4b819f4ea88f5583b8ee24",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "38d5a25073121e8e36ad",
+    "url": "/chat/static/css/main.28283dba.chunk.css"
+  },
+  {
+    "revision": "6ef797a4fc626fee7ff2",
+    "url": "/chat/static/js/2.6f10c6eb.chunk.js"
+  },
+  {
+    "revision": "38d5a25073121e8e36ad",
+    "url": "/chat/static/js/main.de7f90b7.chunk.js"
+  },
+  {
+    "revision": "64af32631123f83d383a",
+    "url": "/chat/static/js/runtime~main.aa8d6e09.js"
+  }
+]);
